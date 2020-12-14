a73bb3d90cf08b3bfac7e39d5cfe7c1d86d66547
Elena Fedoseeva <horungeelena@gmail.com>
2020-12-14 17:43:46 +0200

    task completion manager

---
+++ src/main/java/Manager/PostManager.java
@@ -0,0 +1,16 @@
+package Manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int ownerId, String domain, String query, Boolean ownersOnly, int count, int offset) {
+        // add logic
+        return null;
+    }
+
+    public void delete(int ownerId, int postId){
+        // add logic
+    }
+}
